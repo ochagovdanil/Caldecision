e4b2ca9b264a1f5764a2b7addf48eb2ad3a027ba
Danil_Ochagov <danilochagov@mail.ru>
2018-11-19 14:55:28 +0300

    add junit tests

--- app/src/main/java/com/example/danilochagov/calc_3000/Main/ExpressionsCalculator.java
+++ app/src/main/java/com/example/danilochagov/calc_3000/Main/ExpressionsCalculator.java
@@ -16,29 +16,61 @@ public class ExpressionsCalculator implements ICalculator {
         return number;
     }
 
-    // get one number and number of power from the expression (2^6 -> [2, 6])
+    // get one number and number of power from the expression ('2^6' -> [2, 6])
     double[] getTwoNumbersPower (String text) {
-        double number = Double.parseDouble(text.substring(0, text.indexOf("^")));
-        double pow = Double.parseDouble(text.substring(text.indexOf("^") + 1));
+        if (text.contains("^")) {
+            double number = Double.parseDouble(text.substring(0, text.indexOf("^")));
+            double pow = Double.parseDouble(text.substring(text.indexOf("^") + 1));
+            return new double[]{number, pow};
+        }
 
-        return new double[]{number, pow};
+        throw new IllegalArgumentException("The expression has not got the next symbol - '^'");
     }
 
     double[] getTwoNumbers(String text, String current_operator) {
-        String one, two;
+        if (!current_operator.equals("")) {
+            String one, two;
 
-        // if the expression has two minus
-        if (text.charAt(0) == '-' && current_operator.equals("-")) {
-            int index_operator = 0;
+            // if the expression has two minus
+            if (text.charAt(0) == '-' && current_operator.equals("-")) {
+                int index_operator = 0;
 
-            for (int i = 0; i < text.length(); i++) {
-                if (text.charAt(i) == '-' && i > 0) {
-                    index_operator = i;
+                for (int i = 0; i < text.length(); i++) {
+                    if (text.charAt(i) == '-' && i > 0) {
+                        index_operator = i;
+                    }
                 }
+
+                one = text.substring(0, index_operator);
+                two = text.substring(index_operator + 1, text.length());
+
+                one = checkForPowerStatement(one);
+                two = checkForPowerStatement(two);
+
+                return new double[]{Double.parseDouble(one), Double.parseDouble(two)};
             }
 
-            one = text.substring(0, index_operator);
-            two = text.substring(index_operator + 1, text.length());
+            // if the expression has two plus
+            if (text.charAt(0) == '+' && current_operator.equals("+")) {
+                int index_operator = 0;
+
+                for (int i = 0; i < text.length(); i++) {
+                    if (text.charAt(i) == '+' && i > 0) {
+                        index_operator = i;
+                    }
+                }
+
+                one = text.substring(0, index_operator);
+                two = text.substring(index_operator + 1, text.length());
+
+                one = checkForPowerStatement(one);
+                two = checkForPowerStatement(two);
+
+                return new double[]{Double.parseDouble(one), Double.parseDouble(two)};
+            }
+
+            one = text.substring(0, text.indexOf(current_operator));
+            two = text.substring(text.indexOf(current_operator) + 1, text.length());
 
             one = checkForPowerStatement(one);
             two = checkForPowerStatement(two);
@@ -46,32 +78,7 @@ public class ExpressionsCalculator implements ICalculator {
             return new double[]{Double.parseDouble(one), Double.parseDouble(two)};
         }
 
-        // if the expression has two plus
-        if (text.charAt(0) == '+' && current_operator.equals("+")) {
-            int index_operator = 0;
-
-            for (int i = 0; i < text.length(); i++) {
-                if (text.charAt(i) == '+' && i > 0) {
-                    index_operator = i;
-                }
-            }
-
-            one = text.substring(0, index_operator);
-            two = text.substring(index_operator + 1, text.length());
-
-            one = checkForPowerStatement(one);
-            two = checkForPowerStatement(two);
-
-            return new double[]{Double.parseDouble(one), Double.parseDouble(two)};
-        }
-
-        one = text.substring(0, text.indexOf(current_operator));
-        two = text.substring(text.indexOf(current_operator) + 1, text.length());
-
-        one = checkForPowerStatement(one);
-        two = checkForPowerStatement(two);
-
-        return new double[]{Double.parseDouble(one), Double.parseDouble(two)};
+        throw new IllegalArgumentException("The expression has not got a operator!");
     }
 
     @Override
@@ -91,6 +98,10 @@ public class ExpressionsCalculator implements ICalculator {
 
     @Override
     public double divide(double one, double two) {
+        if (two == 0) {
+            throw new ArithmeticException("Division by zero!");
+        }
+
         return one / two;
     }
 
